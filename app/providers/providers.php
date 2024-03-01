523f8f37b893bc4bb24005770dc3414cfd401a50
Juan Cristobal <juancristobalgd1@gmail.com>
2024-03-01 15:44:05 +0100

    modified:   app/providers/providers.php

--- app/providers/providers.php
+++ app/providers/providers.php
@@ -1,18 +1,28 @@
 <?php
 
 return [
-/**
- * ---------------------------------------------------------------
- *  RAXM
- * ---------------------------------------------------------------
- * 
- * Raxm is a livewire-based library for providing a development
- * experience similar to that of single-page applications
- * (SPA), but without the need to write complex JavaScript
- * code. It allows you to create interactive user interfaces by
- * updating user interface components in response to user
- * actions.
- */
-'raxm' => Axm\Raxm\Raxm::class,
-
+    /**
+     ---------------------------------------------------------------
+       APP PROVIDERS
+     ---------------------------------------------------------------
+     *
+     * Here will be registered the classes of the providers that will 
+     * be loaded at the start of the system, these will only be stored 
+     * in the application container and will be consumed on demand.    
+     **/
+    'app'        => App::class,
+    'config'     => Config::class,
+    'session'    => Session\Session::class,
+    'request'    => Http\Request::class,
+    'response'   => Http\Response::class,
+    'router'     => Http\Router::class,
+    'view'       => Views\View::class,
+    'controller' => App\Controllers\BaseController::class,
+    'database'   => Database\Database::class,
+    'validator'  => Validation\Validator::class,
+    'auth'       => Auth\Auth::class,
+    'console'    => Console\ConsoleApplication::class,
+    'raxm'       => Axm\Raxm\Raxm::class,
+    // 'event'      => EventManager::class,
+    // 'cache'      => Cache\Cache::class,
 ];
